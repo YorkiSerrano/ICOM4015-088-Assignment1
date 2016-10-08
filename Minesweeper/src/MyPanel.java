1303e302f97c313902df5371ea9f15a420eb5742
Gabriel <Gabriel@GabySerrano>
2016-10-08 15:42:24 -0400

    MyPanel Class:
    Grid changed to 9x9
    
    Game Class added

--- Minesweeper/src/MyPanel.java
+++ Minesweeper/src/MyPanel.java
@@ -6,17 +6,17 @@ import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
 	private static final long serialVersionUID = 3426940946811133635L;
-	//--------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------
 	private static final int GRID_X = 42;
 	private static final int GRID_Y = 45;
 	private static final int INNER_CELL_SIZE = 29;
-	private static final int TOTAL_COLUMNS = 10;
-	private static final int TOTAL_ROWS = 10; 
-	
-	public static final int MINE_VALUE = -1;
+	private static final int TOTAL_COLUMNS = 9;
+	private static final int TOTAL_ROWS = 9; 
+	public static final int MINE_VALUE = 1;
 	
 	public int x = -1;
 	public int y = -1;
+	public int totalMines = 10;
 	
 	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
@@ -36,39 +36,40 @@ public class MyPanel extends JPanel {
 		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("TOTAL_ROWS must be at least 3!");
 		}
-		
-		//Paint all grids gray initially
+//-----------------------------------------------------------------------------------------------------------------------------------		
+//Paint all grids gray initially
 		for (int x =0; x < TOTAL_COLUMNS; x++) {   
 			for (int y = 0; y < TOTAL_ROWS; y++) {
 				colorArray[x][y] = Color.LIGHT_GRAY;
 			}
 		}
-		//Assign 0 to all grids
+//-----------------------------------------------------------------------------------------------------------------------------------
+//Assign 0 to all grids
 		for(int x = 0; x < TOTAL_COLUMNS; x++){
 			for(int y = 0; y < TOTAL_ROWS; y++){
 				numberArray[x][y] = 0;
 			}
 		}
-		//Mine Setter
+//-----------------------------------------------------------------------------------------------------------------------------------
+//Mine Setter
 		
 		Random randomGrid = new Random();
-		int mineCount = 10;
-		
-		
-		while(mineCount != 0){
 			
-				int xRandom = randomGrid.nextInt(10);
-				int yRandom = randomGrid.nextInt(10);
-				
-				
-				if(!(numberArray[xRandom][yRandom] == 1 && mineCount != 0)){
-					numberArray[xRandom][yRandom] = 1;
-					System.out.println("Coordinate: (" + xRandom + "," + yRandom + ")\n");
-					
-					mineCount--;
-				}
+		while(totalMines != 0){
+
+			int xRandom = randomGrid.nextInt(9);
+			int yRandom = randomGrid.nextInt(9);
+
+			if(!(numberArray[xRandom][yRandom] == MINE_VALUE && totalMines != 0)){
+				numberArray[xRandom][yRandom] = MINE_VALUE;
+				System.out.println("Coordinate: (" + xRandom + "," + yRandom + ")\n"); //Debugging Purposes
+				totalMines--;
 			}
-	}
+		}
+		}
+	
+//-----------------------------------------------------------------------------------------------------------------------------------
+//Painting
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
@@ -85,27 +86,28 @@ public class MyPanel extends JPanel {
 //-----------------------------------------------------------------------------------------------------------------------------------
 //Paint the background
 		
-		Color colorBackground = new Color(0xA1CAF1);//Baby Blue Eyes https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F
-		g.setColor(colorBackground);
+		Color backColor = new Color(0xA1CAF1);//Baby Blue Eyes https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F
+		g.setColor(backColor);
 		g.fillRect(x1, y1, width + 1, height + 1);
 
 //-----------------------------------------------------------------------------------------------------------------------------------
-//Draw 10 X 10 Grid (TOTAL_COLUMNS X TOTAL_ROWS)
+//Draw 9 X 9 Grid (TOTAL_COLUMNS X TOTAL_ROWS)
 		
 		g.setColor(Color.BLACK);
 		
-		for (int y = 0; y <= TOTAL_ROWS - 1; y++) {
+		for (int y = 0; y <= TOTAL_ROWS; y++) {
 			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
 		}
 		for (int x = 0; x <= TOTAL_COLUMNS; x++) {
-			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)));
+			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS)));
 		}
 //-----------------------------------------------------------------------------------------------------------------------------------
 //Paint cell colors
 		
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
-			for (int y = 0; y < TOTAL_ROWS-1; y++) {
-				if ((x == 0) || (y != TOTAL_ROWS - 1)) {
+			for (int y = 0; y < TOTAL_ROWS; y++) {
+				//if(x != TOTAL_COLUMNS-1 || y!= TOTAL_ROWS-1){
+				if ((x == 0) || (y != TOTAL_ROWS)) {
 					Color c = colorArray[x][y];
 					g.setColor(c);
 					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
@@ -135,7 +137,7 @@ public class MyPanel extends JPanel {
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
 		
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS - 2 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
 			return -1;
 		}
 		return x;
@@ -161,9 +163,10 @@ public class MyPanel extends JPanel {
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
 		
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS - 2 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
 			return -1;
 		}
 		return y;
 	}
+
 }
