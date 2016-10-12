1acd684e9a2d2a38de1a4fe9fff5c97ef9089699
Gabriel <Gabriel@GabySerrano>
2016-10-11 20:45:11 -0400

    Changes made
    *Main
    Removed setGame statement
    
    *MyMouseAdapter
    Edited right press event, replaced instructions with selectGrid method  
    Moved setColor switch statement to MyPanel class
    Edited left press event
    
    *MyPanel
    -Created winGame variable to hold win game status
    -Added instructions to paint numbers and 'You Win!' message on grid
    -countMines method edited and finished.  Name changed to
    getTotalMinesAround
    -Created showMineLocations method to paint all mines on grid
    -Created getWinGameStatus method to check if user won the game
    -Created selectGrid method for left mouse press event
    -Created openSurroundings method to open surrounding blank grids
    recursively
    -lostGame method edited (added showMines method), name changed to
    gameLost.
    -wonGame method edited (added showMines method)
    -setColor switch statement edited and finished.  Name changed to
    setNumberColor.
    -newGame method name changed to resetGame
    -setGame method edited (added reset method)
    -Methods description added
    -Methods organized into setters and getters

--- Minesweeper/src/MyMouseAdapter.java
+++ Minesweeper/src/MyMouseAdapter.java
@@ -7,10 +7,9 @@ import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
 
 public class MyMouseAdapter extends MouseAdapter {
-	private int flag =10;
-	
-//----------------------------------------------------------------------------------------------------------------------------------
-//Mouse Pressed
+	public int flagCounter = 10;
+	//----------------------------------------------------------------------------------------------------------------------------------
+	//Mouse Pressed
 	public void mousePressed(MouseEvent e) {
 		Component c = e.getComponent();
 		while (!(c instanceof JFrame)) {
@@ -37,13 +36,15 @@ public class MyMouseAdapter extends MouseAdapter {
 
 			break;
 		case 3:		
-			
+
 			break;
-		default:    //Some other button (2 = Middle mouse button, etc.)
+		default:    
 			//Do nothing
 			break;
 		}
 	}
+	//----------------------------------------------------------------------------------------------------------------------------------
+	//Mouse Released
 	public void mouseReleased(MouseEvent e) {
 		Component c = e.getComponent();
 		while (!(c instanceof JFrame)) {
@@ -79,66 +80,54 @@ public class MyMouseAdapter extends MouseAdapter {
 						//Released the mouse button on a different cell where it was pressed
 						//Do nothing
 					} else {
-						//Access only hidden grids
-							
-							if(myPanel.mineArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]==1){
-								myPanel.repaint();
-								myPanel.lostGame();
-							}else if(myPanel.numberArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]>=0){
-								myPanel.colorArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]= setColor(myPanel.numberArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]);
-								myPanel.repaint();
-							}
-						}			
-					}
+
+						myPanel.selectGrid(myPanel.mouseDownGridX, myPanel.mouseDownGridY);
+						myPanel.repaint();
+					}			
+				}
 			}
-			myPanel.repaint();
 			break;
-		case 3:		//Right mouse button
-			Color flagColor = Color.MAGENTA;//flag color
-			if ((myPanel.mouseDownGridX >= 0 && gridX < 9) && (myPanel.mouseDownGridY >= 0 && gridY < 9)){
-				
-				if(myPanel.colorArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]==flagColor){
-					//remove flag
-				myPanel.colorArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]=Color.LIGHT_GRAY;
-				myPanel.repaint();
-				
-				}else{
-					//set flag
-					myPanel.colorArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]=flagColor;
-					myPanel.repaint();
-					
-					
+		case 3:		
+			//---------------------------------------------------------------------------------------------------------------------------------
+			//Right mouse button
+			Color flagColor = Color.RED;//flag color
+			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
+				//Had pressed outside
+				//Do nothing
+			} else {
+				if ((gridX == -1) || (gridY == -1)) {
+					//Is releasing outside
+					//Do nothing
+				} else {
+					if ((myPanel.mouseDownGridX != gridX) || (myPanel.mouseDownGridY != gridY)) {
+						//Released the mouse button on a different cell where it was pressed
+						//Do nothing
+					} else {
+
+						if(myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] != Color.WHITE){
+
+
+							if((myPanel.colorArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]==flagColor)){
+								//remove flag
+								myPanel.colorArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]=Color.LIGHT_GRAY;
+								myPanel.repaint();
+
+							}else{
+								//set flag
+								myPanel.colorArray[myPanel.mouseDownGridX][ myPanel.mouseDownGridY]=flagColor;
+								myPanel.repaint();
+
+							}
+						}
+					}
 				}
 			}
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
-			}
 		}
-	public Color setColor(int x){
-		//method for selecting the random color
-		Color newColor = null;
-		switch (x) {
-		case 0:
-			newColor = Color.WHITE;//
-			break;
-		case 1:
-			newColor = Color.RED;//number one
-			break;
-		case 2:
-			newColor = Color.BLUE;//number two
-			break;
-		case 3:
-			newColor = Color.DARK_GRAY;//number three
-			break;
-		case 4:
-			//number 4
-			newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-			break;
-		case 5:
-			newColor = Color.GREEN;//number 5
-		}
-		return newColor;
 	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	
 }
